37cdf556d019e0c9a0be1eee880baaa5223ab742
Pelle Sølvkjær Christensen <PELC@VIAUC.DK>
2024-09-16 22:38:43 +0200

    minefield init

--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -8,11 +8,11 @@ namespace OOP1
         {
 
             #region ugedageogkaffe
-            int[] ugedage = ​{ 7, 5, 3, 3, 5, 1, 6 };
+            int[] ugedage = { 5, 4, 3, 1, 6, 4, 4 };
             int total = 0; 
 
             //vi anvender længden af arrayet 'ugedage' til at 
-            for(int i = 0; i <= ugedage.Length; i++)
+            for(int i = 0; i < ugedage.Length; i++)
             {
                 total += ugedage[i];
             }
@@ -22,9 +22,50 @@ namespace OOP1
             Debug.WriteLine($"Du drak {gennemsnit} kopper kaffe per dag");
             #endregion
 
-            Debug.WriteLine("2: **************");
-            Debug.WriteLine("3: **********");
-            Debug.WriteLine("4: ***************");
+
+            #region minefield
+            int length = 5;
+            int height = 5;
+            float[,] minefield = new float[5, 5];
+
+            for(var x = 0; x < length; x++)
+            {
+                for(var y = 0; y < height; y++)
+                {
+                    Random n = new Random();
+                    minefield[x, y] = float.Round(n.NextSingle(), 3);
+                    //Debug.WriteLine("X " + x + " Y " + y + " " + minefield[x, y]);
+                }
+            }
+
+            string[] lines = new string[height];
+
+            for (var x = 0; x < length; x++)
+            {
+                string line = "*"; 
+                for (var y = 0; y < height; y++)
+                {
+                    char marker = 'o';
+                    if (minefield[x,y] <= 0.1f)
+                    {
+                        marker = '*';
+                    }
+
+                    line += marker;
+          
+                }
+                line += "*";
+                lines[x] = line;
+            }
+
+            //foreach er en forlykke der løber alle variable af en type igennem i et array, hvor man ikke 
+            //kender længde.... den giver dog mere mening ved lister
+            foreach(string line in lines)
+            {
+                Debug.WriteLine(line);
+            }
+
+            #endregion
 
             #region dicefunction
             //Returnér en integer med en tilfældig værdi
@@ -34,7 +75,7 @@ namespace OOP1
                 return r.Next(1, 6);
             }
 
-            #region
+            #endregion
         }
     }
 }
